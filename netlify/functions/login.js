ee3dfa81ad495bf3b87bf879f0035c3ae137a032
3CI_SCARCELLA_FEDERICO_2023_24 <148723040+3CI-SCARCELLA-FEDERICO-2023-24@users.noreply.github.com>
2025-05-08 12:25:20 +0200

    Update login.js

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -1,10 +1,16 @@
-const users = JSON.parse(process.env.USERS_DATA || '{"users":[]}'); 
+const users = JSON.parse(process.env.USERS_DATA || '{"users":[]}');
 const { username, password } = JSON.parse(event.body);
 
 const user = users.users.find((u) => u.username === username && u.password === password);
 
 if (!user) {
-  return { statusCode: 401, body: JSON.stringify({ success: false, message: "Credenziali errate!" }) };
+  return {
+    statusCode: 401,
+    body: JSON.stringify({ success: false, message: "Credenziali errate!" })
+  };
 }
 
-return { statusCode: 200, body: JSON.stringify({ success: true, message: "Login effettuato!" }) };
+return {
+  statusCode: 200,
+  body: JSON.stringify({ success: true, message: "Login effettuato!" })
+};
